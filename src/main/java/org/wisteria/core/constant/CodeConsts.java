8e8cd6ccb408320fb7344b9b4d2bccc62d69e16e
= <=>
2018-01-10 17:55:21 +0800

    功能更新

--- src/main/java/org/wisteria/core/constant/CodeConsts.java
+++ src/main/java/org/wisteria/core/constant/CodeConsts.java
@@ -2,7 +2,7 @@ package org.wisteria.core.constant;
 
 /**
  * 应用级别的代码常量类，适用于所有应用系统
- * @author HuaShengPi
+ * @author huaPi
  *
  */
 public interface CodeConsts {
@@ -10,21 +10,21 @@ public interface CodeConsts {
 	/**
 	 * 未删除的
 	 */
-	Integer UNDELETED = 1;
+	int UNDELETED = 1;
 	
 	/**
 	 * 已删除的
 	 */
-	Integer DELETED = 2;
+	int DELETED = 2;
 
 	/**
 	 * 未激活的
 	 */
-	Integer UNACTIVATED = 100201;
+	int UNACTIVATED = 100201;
 	
 	/**
 	 * 已激活的
 	 */
-	Integer ACTIVATED = 100202;
+	int ACTIVATED = 100202;
 	
 }
